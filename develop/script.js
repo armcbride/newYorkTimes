5dbf17079606a58b30647259dedc1a8cdc06f582
Annaruth McBride <armcbride17@gmail.com>
2020-03-27 12:11:43 -0500

    connects js files and adds API key

--- develop/script.js
+++ develop/script.js
@@ -2,7 +2,7 @@ var searchName = $("#search-term").val().trim();
    
     $("#run-search").on("click", function(event){
         event.preventDefault();
-        var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchName + "api-key=R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" ;
+        var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchName + "api-key=B9Pap5ILlkpGw0xji42v1mq4ErAWc4aA" ;
         $.ajax({
             url: queryUrl,
             method: "GET"
